f4806becaf9cf4c65dd3bd20fbba52e5d1d3be13
Marvin Louie Fabre <marvinlouiefabre@gmail.com>
2021-11-09 09:03:27 +0800

    Updated with app icons and new image assets

--- Piso Flip/ViewController.swift
+++ Piso Flip/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var randomCoinImage : Int = 0
+    let coinArray = ["heads", "tails"]
+    @IBOutlet weak var coinFace: UIImageView!
     override func viewDidLoad() {
+        coinFlip()
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func flipBtn(_ sender: UIButton) {
+        coinFlip()
+    }
+    
+    func coinFlip(){
+        randomCoinImage = Int(arc4random_uniform(2))
+        coinFace.image = UIImage(named: coinArray[randomCoinImage])
+    }
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        coinFlip()
+    }
 }
 
